3ba752ccb6185d9def446575e9ac403519c96134
Julius Alvarado <angularjs.uiux@gmail.com>
2017-11-21 22:03:10 -0800

    just made some modifications

--- ng-register/builds/angularregistration/js/controllers/registration.js
+++ ng-register/builds/angularregistration/js/controllers/registration.js
@@ -1,17 +1,20 @@
-myApp.controller('RegistrationController', 
-  ['$scope', 'Authentication', 
-  function($scope, Authentication) {
+myApp.controller('RegistrationController',
+    ['$scope', 'Authentication', '$location',
+        function ($scope, Authentication, $location) {
+            // $scope.message = "";
+            $scope.user = {};
 
-  $scope.login = function() {
-    Authentication.login($scope.user);
-  };
+            $scope.login = function () {
+                Authentication.login($scope.user);
+            };
 
-  $scope.logout = function() {
-    Authentication.logout();
-  };
+            $scope.logout = function () {
+                $location.path('/login');
+                Authentication.logout();
+            };
 
-  $scope.register = function() {
-    Authentication.register($scope.user);
-  }; //register
+            $scope.register = function () {
+                Authentication.register($scope.user);
+            }; // register
 
-}]); //Controller
+        }]); // Controller
--- ng-register/builds/angularregistration/js/services/authentication.js
+++ ng-register/builds/angularregistration/js/services/authentication.js
@@ -1,63 +1,63 @@
 myApp.factory('Authentication',
-  ['$rootScope', '$location', '$firebaseObject', '$firebaseAuth',
-  function($rootScope, $location, $firebaseObject, $firebaseAuth) {
+    ['$rootScope', '$location', '$firebaseObject', '$firebaseAuth',
+        function ($rootScope, $location, $firebaseObject, $firebaseAuth) {
 
-  var ref = firebase.database().ref();
-  var auth = $firebaseAuth();
-  var myObject;
+            var ref = firebase.database().ref();
+            var auth = $firebaseAuth();
+            var authapi;
 
-  auth.$onAuthStateChanged(function(authUser) {
-    if(authUser) {
-      var userRef = ref.child('users').child(authUser.uid);
-      var userObj = $firebaseObject(userRef);
-      $rootScope.currentUser = userObj;
-    } else {
-      $rootScope.currentUser = '';
-    }
-  });
+            auth.$onAuthStateChanged(function (authUser) {
+                console.log("jha - there was a change to auth state...");
+                if (authUser) {
+                    var userRef = ref.child('users').child(authUser.uid);
+                    $rootScope.currentUser = $firebaseObject(userRef);
+                } else {
+                    $rootScope.currentUser = '';
+                }
+            });
 
-  myObject = {
-    login: function(user) {
-      auth.$signInWithEmailAndPassword(
-        user.email,
-        user.password
-      ).then(function(user) {
-        $location.path('/success');
-      }).catch(function(error) {
-        $rootScope.message = error.message;
-      }); //signInWithEmailAndPassword
-    }, //login
+            authapi = {
+                login: function (user) {
+                    auth.$signInWithEmailAndPassword(
+                        user.email,
+                        user.password
+                    ).then(function (user) {
+                        $location.path('/success');
+                    }).catch(function (error) {
+                        $rootScope.message = error.message;
+                    }); //signInWithEmailAndPassword
+                }, //login
 
-    logout: function() {
-      return auth.$signOut();
-    }, //logout
+                logout: function () {
+                    return auth.$signOut();
+                }, //logout
 
-    requireAuth: function() {
-      return auth.$requireSignIn();
-    }, //require Authentication
+                requireAuth: function () {
+                    return auth.$requireSignIn();
+                }, // require Authentication
 
-    register: function(user) {
-      auth.$createUserWithEmailAndPassword(
-        user.email,
-        user.password
-      ).then(function(regUser) {
-        var regRef = ref.child('users')
-          .child(regUser.uid).set({
-            date: firebase.database.ServerValue.TIMESTAMP,
-            regUser: regUser.uid,
-            firstname: user.firstname,
-            lastname: user.lastname,
-            email: user.email
-          }); //userinfo
-          myObject.login(user);
-      }).catch(function(error) {
-        $rootScope.message = error.message;
-      }); //createUserWithEmailAndPassword
-    } //register
+                register: function (user) {
+                    auth.$createUserWithEmailAndPassword(
+                        user.email,
+                        user.password
+                    ).then(function (regUser) {
+                        var regRef = ref.child('users').child(regUser.uid).set({
+                                date: firebase.database.ServerValue.TIMESTAMP,
+                                regUser: regUser.uid,
+                                firstname: user.firstname,
+                                lastname: user.lastname,
+                                email: user.email
+                            }); // user info
+                        $rootScope.message = "Thanks for registering "+user.firstname;
+                        authapi.login(user);
+                    }).catch(function (error) {
+                        $rootScope.message = error.message;
+                    }); //createUserWithEmailAndPassword
+                } //register
 
-  }; //return
+            }; //return
 
 
-  return myObject;
+            return authapi;
 
-}]); //factory
+        }]); //factory
