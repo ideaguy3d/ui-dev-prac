d249c8f78e83dcd023b7f91a751aed36b2dc61d6
Julius Alvarado <angularjs.uiux@gmail.com>
2017-11-23 20:06:05 -0800

    working on a 'show love' feature

--- ng-register/builds/angularregistration/js/controllers/checkins.js
+++ ng-register/builds/angularregistration/js/controllers/checkins.js
@@ -4,14 +4,25 @@
 myApp.controller('CheckInsController', ['$rootScope', '$scope', '$routeParams', '$firebaseObject',
     '$location', '$firebaseArray',
     function ($rootScope, $scope, $routeParams, $firebaseObject, $location, $firebaseArray) {
-        var ref;
         $scope.whichmeeting = $routeParams.meetingId;
         $scope.whichuser = $routeParams.userId;
-
-        ref = firebase.database().ref().child('users').child($scope.whichuser)
+        $scope.order = 'firstname';
+        $scope.direction = null;
+        $scope.query = '';
+        $scope.recordId = '';
+        var ref = firebase.database().ref().child('users').child($scope.whichuser)
             .child('meetings').child($scope.whichmeeting).child('checkins');
+        var checkinsList = $firebaseArray(ref);
+        $scope.checkins = checkinsList;
 
-        $scope.checkins = $firebaseArray(ref);
+        $scope.showLove = function(myCheckin){
+            myCheckin.show = !myCheckin.show; 
+        };
+
+        $scope.pickRandom = function () {
+            var whichRecord = Math.round(Math.random() * (checkinsList.length - 1))
+            $scope.recordId = checkinsList.$keyAt(whichRecord);
+        };
 
         $scope.addCheckin = function () {
             $firebaseArray(ref).$add({
@@ -19,14 +30,14 @@ myApp.controller('CheckInsController', ['$rootScope', '$scope', '$routeParams', 
                 lastname: $scope.user.lastname,
                 email: $scope.user.email,
                 date: firebase.database.ServerValue.TIMESTAMP
-            }).then(function(res){
+            }).then(function (res) {
                 console.log("jha - response after adding data via fbArr = ");
                 console.log(res);
-                $location.path('/checkins/'+$scope.whichuser+'/'+$scope.whichmeeting+'/checkinsList');
+                $location.path('/checkins/' + $scope.whichuser + '/' + $scope.whichmeeting + '/checkinsList');
             });
         };
 
-        $scope.deleteCheckin = function(id){
+        $scope.deleteCheckin = function (id) {
             var refDel = ref.child(id);
             var recordObj = $firebaseObject(refDel);
             recordObj.$remove(id);
